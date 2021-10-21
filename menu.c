f297bb9d5e72356e54553cf54cbb5dbf9c2312eb
yOurOck-wallcrew <55618130+yOurOck-wallcrew@users.noreply.github.com>
2021-10-21 17:08:17 +0300

    new menu

--- menu.c
+++ menu.c
@@ -1,234 +1,143 @@
-
-// #include <ncurses.h>
-// #include <stdlib.h>
-// #include <string.h>
-
-// int		main(void)
-// {
-// 	int		win_x; //Max Win X
-// 	int		win_y; //Max Win Y
-// 	int		option = 1; //Current Selected Option
-// 	int		c_x; //Center X of screen
-// 	int		c_y; //Center Y of screen
-// 	int		titleboxlen; //Strlen of titlebox
-// 	titleboxlen = strlen("=========================================================================="); //Get Title Box Length To Center It
-
-// 	char *o_1p = "1 Player"; //Just The Option Names, You Can Change Em
-// 	char *o_2p = "2 Players";
-// 	char *o_help = "Help";
-// 	char *o_credits = "Credits";
-// 	char *o_quit = "Quit";
-
-// 	initscr();
-// 	cbreak();
-// 	noecho();
-// 	curs_set(FALSE);
-// 	start_color();
-// 	init_pair(1, COLOR_BLACK, COLOR_WHITE);
-
-// 	getmaxyx(stdscr, win_y, win_x);
-// 	WINDOW *win_menu = newwin(win_y, win_x, 0, 0);
-
-// 	keypad(win_menu, true);
-
-// 	while (1)
-// 	{
-// 		wclear(win_menu);
-// 		getmaxyx(win_menu, win_y, win_x);
-// 		c_x = win_x / 2; //Find Middle of X
-// 		c_y = win_y / 2; //Find Middle of Y
-
-// 		//Render Fancy Boxes ETC
-// 		//The Text centering takes half the screen, then further subtracts half the strlen to center it perfectly. Just for you mister OCD
-// 		mvwprintw(win_menu, c_y - 10, c_x - (titleboxlen / 2), "==========================================================================");
-// 		mvwprintw(win_menu, c_y - 9, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y - 8, c_x - (titleboxlen / 2), "||                                TETRIS                                ||");
-// 		mvwprintw(win_menu, c_y - 7, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y - 6, c_x - (titleboxlen / 2), "==========================================================================");
-// 		mvwprintw(win_menu, c_y - 5, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y - 4, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y - 3, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y - 2, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y - 1, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 1, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 2, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 3, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 4, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 5, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 6, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 7, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 8, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 9, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 10, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 11, c_x - (titleboxlen / 2), "||                                                                      ||");
-// 		mvwprintw(win_menu, c_y + 12, c_x - (titleboxlen / 2), "==========================================================================");
-
-// 		//Render Out Buttons, Inverse Video Of Selected
-// 		if (option == 1)
-// 		{
-// 			wattron(win_menu, COLOR_PAIR(1));
-// 			mvwprintw(win_menu, c_y - 1, c_x - (strlen(o_1p) / 2), o_1p);
-// 			wattroff(win_menu, COLOR_PAIR(1));
-// 		}
-// 		else
-// 			mvwprintw(win_menu, c_y - 1, c_x - (strlen(o_1p) / 2), o_1p);
-
-// 		if (option == 2)
-// 		{
-// 			wattron(win_menu, COLOR_PAIR(1));
-// 			mvwprintw(win_menu, c_y + 1, c_x - (strlen(o_2p) / 2), o_2p);
-// 			wattroff(win_menu, COLOR_PAIR(1));
-// 		}
-// 		else
-// 			mvwprintw(win_menu, c_y + 1, c_x - (strlen(o_2p) / 2), o_2p);
-
-// 		if (option == 3)
-// 		{
-// 			wattron(win_menu, COLOR_PAIR(1));
-// 			mvwprintw(win_menu, c_y + 3, c_x - (strlen(o_help) / 2), o_help);
-// 			wattroff(win_menu, COLOR_PAIR(1));
-// 		}
-// 		else
-// 			mvwprintw(win_menu, c_y + 3, c_x - (strlen(o_help) / 2), o_help);
-
-// 		if (option == 4)
-// 		{
-// 			wattron(win_menu, COLOR_PAIR(1));
-// 			mvwprintw(win_menu, c_y + 5, c_x - (strlen(o_credits) / 2), o_credits);
-// 			wattroff(win_menu, COLOR_PAIR(1));
-// 		}
-// 		else
-// 			mvwprintw(win_menu, c_y + 5, c_x - (strlen(o_credits) / 2), o_credits);
-
-// 		if (option == 5)
-// 		{
-// 			wattron(win_menu, COLOR_PAIR(1));
-// 			mvwprintw(win_menu, c_y + 7, c_x - (strlen(o_quit) / 2), o_quit);
-// 			wattroff(win_menu, COLOR_PAIR(1));
-// 		}
-// 		else
-// 			mvwprintw(win_menu, c_y + 7, c_x - (strlen(o_quit) / 2), o_quit);
-
-// 		//Update Screen
-// 		wrefresh(win_menu);
-
-// 		//Key Hooks
-// 		switch(wgetch(win_menu))
-// 		{
-// 			case KEY_UP:
-// 				if (option == 1)
-// 					option = 5;
-// 				else
-// 					option -= 1;
-// 				break;
-// 			case KEY_DOWN:
-// 				if (option == 5)
-// 					option = 1;
-// 				else
-// 					option += 1;
-// 				break;
-// 			case 10: //10 Being Enter Key
-// 				if (option == 1)
-// 				{
-// 					//Do One Player Stuff
-// 				}
-
-// 				if (option == 2)
-// 				{
-// 					//Do Two Player Stuff
-// 				}
-
-// 				if (option == 3)
-// 				{
-// 					//Help Stuff
-// 				}
-
-// 				if (option == 4)
-// 				{
-// 					//Credits Stuff
-// 				}
-
-// 				if (option == 5)
-// 				{
-// 					//Quit The Game
-// 					endwin();
-// 					curs_set(TRUE);
-// 					system("clear");
-// 					return (0);
-// 				}
-// 				break;
-// 		}
-// 	}
-// }
-
 #include <ncurses.h>
-#include <string.h>
-#include <stdio.h>
-
+#include <stdbool.h>
+#define height 20
+#define start_y 17
+#define width 30
+#define start_x 45
+char up = '=';
+char side = '|';
+int x = 0;
+int i;
+int smth = 0;
+int counter = 0;
+int ishod_start = 0;
+int ishod_level = 1;
+int ishod_credits = 2;
+int ishod_quit = 3;
 int main()
 {
-  char add;
 
-  int y = 20;
-  int x = 45;
-  initscr();
-  raw();
-  cbreak();
-  noecho();
-  curs_set(FALSE);
+    char *options[4] = {"Start", "Level", "Credits", "Quit"};
 
-  if (has_colors() == FALSE)
-  {
-    endwin();
-    printf("Your terminal does not support color\n");
-  }
-  start_color();
-  init_pair(1, COLOR_BLACK, COLOR_WHITE);
-
-  while (y != 30)
-  {
-    mvprintw(20, 45, "==========================");
-    mvprintw(30, 45, "==========================");
-    mvaddch(y, x, '|');
-    mvaddch(y, x + 25, '|');
-    refresh();
-    ++y;
-  }
-  char *message_controls = "Controls";
-  char *message_quit = "Quit";
-  char *message_start = "Start the game";
-
-  mvprintw(22, 50, message_start);
-  mvprintw(24, 50, message_controls);
-  mvprintw(26, 50, message_quit);
-
-  while ((add = getch()) != 's' || (add = getch()) != 'c' || (add = getch()) != 'q')
-  {
-    switch (add)
+    initscr();
+    cbreak();
+    noecho();
+    if (has_colors() != true)
     {
-    case 's':
 
-      mvprintw(22, 50, message_start);
-
-      break;
-
-    case 'c':
-
-      mvprintw(24, 50, message_controls);
-
-      break;
-
-    case 'q':
-
-      mvprintw(26, 50, message_quit);
-
-      break;
+        printw("Your terminal does not support colors");
     }
+
+    start_color();
+    init_pair(1, COLOR_MAGENTA, COLOR_CYAN);
+    init_pair(2, COLOR_BLACK, COLOR_BLACK);
+
+    WINDOW *win = newwin(height+10, width + 2, start_y, start_x);
     refresh();
-  }
 
-  endwin();
+    wborder(win, (int)side, (int)side, (int)up, (int)up, (int)up, (int)up, (int)up, (int)up);
+    nodelay(win, TRUE);
+    keypad(win, true);
 
-  return 0;
+    wrefresh(win);
+
+    int choise = wgetch(win);
+
+    int c = wgetch(win);
+    int a = wgetch(win);
+    while (true)
+    {
+        mvwprintw(win, i + 5, x + 11, options[i]);
+        while (1)
+        {
+            for (i = 0; i < 4; i++)
+            {
+                if (i == smth)
+                {
+
+                    wattron(win, COLOR_PAIR(1));
+                    mvwprintw(win, i + 5, x + 11, options[i]);
+                    wattroff(win, COLOR_PAIR(1));
+                }
+
+                choise = wgetch(win);
+
+                switch (choise)
+                {
+
+                case KEY_UP:
+                    if (counter > 0)
+                    {
+
+                        counter = counter - 1;
+                    }
+                    smth--;
+                    break;
+
+                case KEY_DOWN:
+                    if (counter < 3)
+                    {
+
+                        counter = counter + 1;
+                    }
+
+                    smth++;
+                    break;
+
+                case 10:
+
+                    x = smth;
+                    if (ishod_start == smth)
+                    {
+
+                        while (1)
+                        {
+                           clear();
+                           refresh();
+                            mvwprintw(win, 1, 1, "the game has started");
+                            refresh();
+                            break;
+
+                            // ВЕСЬ ЭТОТ ПРИКОЛ ДОЛЖЕН ЗАПУСКАТЬ ИГРУ
+                        }
+                        break;
+                    }
+                    if (ishod_quit == smth)
+                    {
+
+                        
+                        
+                            exit(0);
+
+                            // ВЕСЬ ЭТОТ ПРИКОЛ ДОЛЖЕН ЗАПУСКАТЬ ИГРУ
+                        
+                    }
+                    if (ishod_credits == smth)
+                    {
+                        clear();
+                           refresh();
+                            mvwprintw(win, 1, 1, "yarutiunov, fstaryk, hpinchuktr");
+                            refresh();
+                            break;
+                            
+
+                            // ВЕСЬ ЭТОТ ПРИКОЛ ДОЛЖЕН ЗАПУСКАТЬ ИГРУ
+                    }
+                    
+                
+                }
+            }
+
+            if (choise == 27)
+            {
+
+                endwin();
+                return 0;
+            }
+              
+        }
+      endwin();
+    return 0;
+    }
 }
+
